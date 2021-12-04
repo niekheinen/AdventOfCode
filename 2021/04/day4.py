e50890fb839b7892afba11dee7484dc67fa45d10
Niek <niekheinen@gmail.com>
2021-12-04 16:40:15 +0100

    ik push ff brakke code zodat ik kan proggen vanaf mn telefoon

--- 2021/04/day4.py
+++ 2021/04/day4.py
@@ -18,6 +18,8 @@ class Board():
         return sum(n for n in temp if n != 'x')
 
 
+
+
 def puzzle1(lines, numbers):
     boards = [Board(line) for line in lines]
     for n in numbers:
@@ -33,11 +35,46 @@ def puzzle2(lines, numbers):
     return score
 
 
+def puzzle1_golf():
+    f = '2021/04/input.txt'
+    l = open(f).read().split('\n\n')
+    nums = [int(n) for n in lines.pop(0).split(',')]
+    j = 0
+    callnum = lambda board, call: board.replace(call, f)
+    haswon = lambda board : 0
+    calcscore = lambda board, call: sum(n for n in board if n!=f) * call if haswon(board) else 0
+    
+    
+    while (score := max([callscore(b) for b in boards])) == 0:
+        boards = [b for b in boards]
+    print(score)
+
+    scores = [[i * b.replace('\n', ' ').split() for i in n] for b in l]
+    
+
+
+    def call_num(board, num):
+        board = [['x' if n == num else n for n in col] for col in board]
+        for i in range(5):
+            if board[i] == ['x'] * 5 or [col[i] for col in board] == ['x'] * 5:
+                return sum_unmarked() * num
+        return False
+
+    def sum_unmarked(board):
+        temp = []
+        for col in board:
+            temp += col
+        return sum(n for n in temp if n != 'x')
+    return 0
+
 if __name__=='__main__':
     with open('2021/04/input.txt') as file:
         lines = file.read().split('\n\n')    
 
     numbers = [int(n) for n in lines.pop(0).split(',')]
    
-    print(f'Puzzle 1 answer: {puzzle1(lines, numbers)}')
+    print(f'Puzzle 1 answer: {(ans1 := puzzle1(lines, numbers))}')
     print(f'Puzzle 2 answer: {puzzle2(lines, numbers)}')
+    
+    assert ans1 == 35711
+    #assert ans1 == puzzle1_golf()
